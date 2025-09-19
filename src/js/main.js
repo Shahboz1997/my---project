f1ca60daffddfe3e148c0c71347f7403aac7264d
Shahboz1997 <sashabilov25@gmail.com>
2025-09-20 00:05:32 +0500

    WIIP

--- src/js/main.js
+++ src/js/main.js
@@ -19,28 +19,47 @@ surveyCollection.forEach(item => {
         text.classList.toggle("d-none");
     });
 })
-console.log('Helloworld');
-const menuWrapper = headerWrapper.querySelector(".menu-wrapper");
-const burger = pageheader.querySelector(".burger");
-const closeButton = pageheader.querySelector(".button-close");
+const rangeWrapper = document.querySelector('.range-wrapper');
+const rangeInput = rangeWrapper.querySelector('#customRange');
+const sliderFill = rangeWrapper.querySelector('.slider-fill');
+const sliderTrumb = rangeWrapper.querySelector('.slider-trumb');
+const rangeValue = rangeWrapper.querySelector('.range-value');
 
-burgerBtnClickHandler = () => {
-  menuWrapper.classList.add("d-flex");
-  menuWrapper.classList.remove("d-none");
-};
-
-btnCloseClickHandler = () => {
-  menuWrapper.classList.remove("d-flex");
-  menuWrapper.classList.add("d-none");
-};
-if (window.innerWidth < 1357) {
-  console.log("mobile");
-  menuWrapper.classList.remove("d-flex");
-  menuWrapper.classList.add("d-none");
-
-  burger.classList.remove("d-none");
-  burger.classList.add("d-flex");
-
-  burger.addEventListener("click", burgerBtnClickHandler);
-  closeButton.addEventListener("click", btnCloseClickHandler);
+const rangeChangeHandler = (event) => {
+    const rangeElement = event.target;
+    const value = (rangeElement.value - rangeElement.min) / (rangeElement. max - rangeElement.min) * 100;
+    sliderFill.style.width = value + '%'; 
+    sliderTrumb.style.left = value + '%';
+    rangeValue.textContent = value.toFixed();
 }
+
+rangeInput.addEventListener('input', (evt) => rangeChangeHandler(evt));
+
+
+
+// console.log('Helloworld');
+// const menuWrapper = headerWrapper.querySelector(".menu-wrapper");
+// const burger = pageheader.querySelector(".burger");
+// const closeButton = pageheader.querySelector(".button-close");
+
+// burgerBtnClickHandler = () => {
+//   menuWrapper.classList.add("d-flex");
+//   menuWrapper.classList.remove("d-none");
+// };
+
+// btnCloseClickHandler = () => {
+//   menuWrapper.classList.remove("d-flex");
+//   menuWrapper.classList.add("d-none");
+// };
+// if (window.innerWidth < 1357) {
+//   console.log("mobile");
+//   menuWrapper.classList.remove("d-flex");
+//   menuWrapper.classList.add("d-none");
+
+//   burger.classList.remove("d-none");
+//   burger.classList.add("d-flex");
+
+//   burger.addEventListener("click", burgerBtnClickHandler);
+//   closeButton.addEventListener("click", btnCloseClickHandler);
+//}
+ //<input class="visually-hidden" type="range" id="amount" name="cash" min="1000" max="25000" value="5000" />
