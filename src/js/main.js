6b86f6a370c86a0b2330d10351fd33c3ec977ac1
Shahboz1997 <sashabilov25@gmail.com>
2025-10-02 15:13:59 +0500

    WIIP

--- src/js/main.js
+++ src/js/main.js
@@ -1,68 +1,61 @@
-console.log('Hello world');
-const surveyContainer = document.querySelector(".survey-page");
-const surveyCollection = surveyContainer.querySelectorAll(".survey-item");
+// console.log('Hello world');
+// const surveyContainer = document.querySelector(".survey-page");
+// const surveyCollection = surveyContainer.querySelectorAll(".survey-item");
 
-surveyCollection.forEach(item => {
-    item.addEventListener("click", (evt) => { 
-        console.log("item", item);
-        const lorem= item.querySelector(".survey-text"); 
-        if (!lorem.classList.contains("d-none")) {
-            lorem.classList.remove("d-none");
-        }
-        else {
-      lorem.classList.add("d-none");
-    };
-        console.log("evt", evt.target);
-        const element = evt.target;
-        const parent = element.closest(".survey-item");
-        const text = parent.querySelector(".survey-text");
-        text.classList.toggle("d-none");
-    });
-})
+// surveyCollection.forEach(item => {
+//     item.addEventListener("click", (evt) => { 
+//         console.log("item", item);
+//         const lorem= item.querySelector(".survey-text"); 
+//         if (!lorem.classList.contains("d-none")) {
+//             lorem.classList.remove("d-none");
+//         }
+//         else {
+//       lorem.classList.add("d-none");
+//     };
+//         console.log("evt", evt.target);
+//         const element = evt.target;
+//         const parent = element.closest(".survey-item");
+//         const text = parent.querySelector(".survey-text");
+//         text.classList.toggle("d-none");
+//     });
+// })
 
 
-const rangeWrapper = document.querySelector('.range-wrapper');
-const rangeInput = rangeWrapper.querySelector('#customRange');
-const sliderFill = rangeWrapper.querySelector('.slider-fill');
-const sliderTrumb = rangeWrapper.querySelector('.slider-trumb');
-const rangeValue = rangeWrapper.querySelector('.range-value');
+// const rangeWrapper = document.querySelector('.range-wrapper');
+// const rangeInput = rangeWrapper.querySelector('#customRange');
+// const sliderFill = rangeWrapper.querySelector('.slider-fill');
+// const sliderTrumb = rangeWrapper.querySelector('.slider-trumb');
+// const rangeValue = rangeWrapper.querySelector('.range-value');
 
-const rangeChangeHandler = (event) => {
-    const rangeElement = event.target;
-    const value = (rangeElement.value - rangeElement.min) / (rangeElement. max - rangeElement.min) * 30000;
-    sliderFill.style.width = value/78.94  + 'px'; 
-    sliderTrumb.style.left = value/78.94  + 'px';
-    rangeValue.textContent = value.toFixed();
+// const rangeChangeHandler = (event) => {
+//     const rangeElement = event.target;
+//     const value = (rangeElement.value - rangeElement.min) / (rangeElement. max - rangeElement.min) * 30000;
+//     sliderFill.style.width = value/78.94  + 'px'; 
+//     sliderTrumb.style.left = value/78.94  + 'px';
+//     rangeValue.textContent = value.toFixed();
+// }
+
+// rangeInput.addEventListener('input', (evt) => rangeChangeHandler(evt));
+
+
+const hamburger = document.querySelector(".hamburger");
+console.log(hamburger);
+const navMenu = document.querySelector(".nav-menu");
+console.log(navMenu);
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+hamburger.classList.toggle("active");
+navMenu.classList.toggle("active");
 }
 
-rangeInput.addEventListener('input', (evt) => rangeChangeHandler(evt));
 
+const navLink = document.querySelectorAll(".nav-link");
 
+navLink.forEach(n => n.addEventListener("click", closeMenu));
 
-
-// console.log('Helloworld');
-// const menuWrapper = headerWrapper.querySelector(".menu-wrapper");
-// const burger = pageheader.querySelector(".burger");
-// const closeButton = pageheader.querySelector(".button-close");
-
-// burgerBtnClickHandler = () => {
-//   menuWrapper.classList.add("d-flex");
-//   menuWrapper.classList.remove("d-none");
-// };
-
-// btnCloseClickHandler = () => {
-//   menuWrapper.classList.remove("d-flex");
-//   menuWrapper.classList.add("d-none");
-// };
-// if (window.innerWidth < 1357) {
-//   console.log("mobile");
-//   menuWrapper.classList.remove("d-flex");
-//   menuWrapper.classList.add("d-none");
-
-//   burger.classList.remove("d-none");
-//   burger.classList.add("d-flex");
-
-//   burger.addEventListener("click", burgerBtnClickHandler);
-//   closeButton.addEventListener("click", btnCloseClickHandler);
-//}
- //<input class="visually-hidden" type="range" id="amount" name="cash" min="1000" max="25000" value="5000" />
+function closeMenu() {
+hamburger.classList.remove("active");
+navMenu.classList.remove("active");
+}
